35d4c1a71d86fee5ab7def6ba66d47dad6c4bc40
didadadida93 <didadadida93@gmail.com>
2019-08-22 06:56:05 +0700

    update * add http module * add unittest for http module

---
+++ t5apy/http.py
@@ -0,0 +1,35 @@
+""" Module `http.py` is for `t5apy` make HTTP request to Travian 5 API
+end points.
+"""
+
+import urllib.request
+import urllib.parse
+
+from . import __version__
+from . import __name__
+
+
+def request(method, url, params={}):
+    """ Make HTTP request to URL.
+
+    Parameters:
+        method: `string` HTTP method.
+        url: `string` URL.
+        params: `dict` passing data to URL's query strings.
+    """
+
+    headers = {
+        'User-Agent': 'python-%s/%s' % (__name__, __version__),
+    }
+
+    if params:
+        url = '?'.join([url, urllib.parse.urlencode(params)])
+
+    req = urllib.request.Request(
+        url=url,
+        method=method,
+        headers=headers,
+    )
+
+    with urllib.request.urlopen(req, timeout=60.0) as r:
+        return r.read()
---
+++ tests/test_http.py
@@ -0,0 +1,51 @@
+import unittest
+from unittest.mock import Mock, patch
+
+from t5apy import __name__
+from t5apy import __version__
+from t5apy.http import request
+
+
+class TestHTTP(unittest.TestCase):
+
+    def setUp(self):
+        self.headers = {
+            'User-Agent': 'python-%s/%s' % (__name__, __version__),
+        }
+        self.url = 'https://httpbin.org/get'
+
+    @patch('t5apy.http.urllib.request')
+    def test_request(self, mock_request):
+        mock_request.urlopen().__enter__().read.return_value = 'mocked'
+
+        r = request('GET', self.url)
+
+        mock_request.Request.assert_called_with(
+            url=self.url,
+            method='GET',
+            headers=self.headers,
+        )
+        mock_request.urlopen.assert_called_with(
+            mock_request.Request(),
+            timeout=60.0,
+        )
+        self.assertEqual(r, 'mocked')
+
+        # testing again with params
+        params = {'a':'a', 'b':'b', 'c':'c'}
+        r = request('GET', self.url, params)
+
+        mock_request.Request.assert_called_with(
+            url=self.url + '?a=a&b=b&c=c',
+            method='GET',
+            headers=self.headers
+        )
+        mock_request.urlopen.assert_called_with(
+            mock_request.Request(),
+            timeout=60.0,
+        )
+        self.assertEqual(r, 'mocked')
+
+
+if __name__ == '__main__':
+    unittest.run()
